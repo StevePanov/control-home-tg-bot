3d2e40162efba67f4e10847be8d0efab74741f22
Степан Панов <panov.step1k@yandex.ru>
2019-05-23 04:46:39 +0500

    fix

--- index.js
+++ index.js
@@ -128,6 +128,6 @@ app.get("/test", (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
-});
+// app.listen(3000, () => {
+//   console.log("Example app listening on port 3000!");
+// });
