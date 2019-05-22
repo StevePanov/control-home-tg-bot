550f0fe8c86bb9a11f512f85a878a3ee1c8a15aa
Степан Панов <panov.step1k@yandex.ru>
2019-05-23 04:55:55 +0500

    fix port

--- index.js
+++ index.js
@@ -131,3 +131,8 @@ app.get("/test", (req, res) => {
 // app.listen(3000, () => {
 //   console.log("Example app listening on port 3000!");
 // });
+const host = "0.0.0.0";
+const port = process.env.PORT || 5000;
+app.listen(port, host, () => {
+  console.log("Listening on Port ", port);
+});
