8ba0a479ccf6c060fb41ff3ba156948a05422761
Степан Панов <panov.step1k@yandex.ru>
2019-06-13 14:16:41 +0500

    add other sensor

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ var chatsId = [];
 
 var hall = 1; //данные датчика Холла
 var infrared = 0; //данные ИК датчика
+var mq2 = 0; //данные mq2
+var mq4 = 0; //данные mq4
 
 var history = {
   //хранение времени и даты для статистики
@@ -17,6 +19,14 @@ var history = {
     LOW: []
   },
   infrared: {
+    HIGH: [],
+    LOW: []
+  },
+  mq2: {
+    HIGH: [],
+    LOW: []
+  },
+  mq4: {
     HIGH: [],
     LOW: []
   }
@@ -36,7 +46,7 @@ bot.onText(
 );
 
 bot.onText(
-  /\/door/,
+  /\/дверь/,
   (onText = msg => {
     console.log("/door  пришло сообщение, hall: ", hall);
     if (hall === 0) {
@@ -50,10 +60,11 @@ bot.onText(
     }
   })
 );
+
 bot.onText(
-  /\/space/,
+  /\/движение/,
   (onText = msg => {
-    console.log("/space  пришло сообщение, infrared: ", infrared);
+    console.log("/движение  пришло сообщение, infrared: ", infrared);
     if (infrared === 1) {
       chatsId.map(id => {
         bot.sendMessage(id, "В зоне датчика движение");
@@ -67,7 +78,59 @@ bot.onText(
 );
 
 bot.onText(
-  /\/stats/,
+  /\/дым/,
+  (onText = msg => {
+    console.log("/дым  пришло сообщение, mq2: ", mq2);
+    if (mq2 === 1) {
+      chatsId.map(id => {
+        bot.sendMessage(id, "Задымление ❗️❗️❗️");
+      });
+    } else {
+      chatsId.map(id => {
+        bot.sendMessage(id, "Дым не обнаружен");
+      });
+    }
+  })
+);
+
+bot.onText(
+  /\/газ/,
+  (onText = msg => {
+    console.log("/газ  пришло сообщение, mq4: ", mq4);
+    if (mq4 === 1) {
+      chatsId.map(id => {
+        bot.sendMessage(id, "Обнаружена утечка газа ❗️ ❗️ ❗️️");
+      });
+    } else {
+      chatsId.map(id => {
+        bot.sendMessage(id, "Утечка газа не зафиксирована");
+      });
+    }
+  })
+);
+
+bot.onText(
+  /\/температура/,
+  (onText = msg => {
+    console.log("/temp  пришло сообщение, temp: ", temp);
+    chatsId.map(id => {
+      bot.sendMessage(id, `Температура: ${temp} градусов`);
+    });
+  })
+);
+
+bot.onText(
+  /\/влажность/,
+  (onText = msg => {
+    console.log("/humidity  пришло сообщение, humidity: ", humidity);
+    chatsId.map(id => {
+      bot.sendMessage(id, `Влажность: ${humidity}%`);
+    });
+  })
+);
+
+bot.onText(
+  /\/статистика/,
   (onText = msg => {
     chatsId.map(id => {
       bot.sendMessage(
@@ -75,7 +138,11 @@ bot.onText(
         `Всего изменений: ${history.hall.HIGH.length +
           history.hall.LOW.length +
           history.infrared.HIGH.length +
-          history.infrared.LOW.length}`
+          history.infrared.LOW.length +
+          history.mq2.HIGH.length +
+          history.mq2.LOW.length +
+          history.mq4.HIGH.length +
+          history.mq4.LOW.length}`
       );
     });
   })
@@ -108,16 +175,76 @@ app.get("/infrared", (req, res) => {
   if (infrared != req.headers.infrared) {
     if (req.headers.infrared == 1) {
       chatsId.map(id => {
-        bot.sendMessage(id, "В зоне датчика движение");
+        bot.sendMessage(id, "Движение обнаружено");
       });
       infrared = req.headers.infrared;
       history.infrared.HIGH.push(new Date());
     } else {
       chatsId.map(id => {
-        bot.sendMessage(id, "В зоне датчика движение не обнажружено");
+        bot.sendMessage(id, "Движение не обнаружено");
       });
       infrared = req.headers.infrared;
       history.infrared.LOW.push(new Date());
+    }
+  }
+  res.sendStatus(200);
+});
+
+app.get("/mq2", (req, res) => {
+  console.log("Запрос /mq2, req.headers.mq2: ", req.headers.mq2);
+
+  if (mq2 != req.headers.mq2) {
+    if (req.headers.mq2 == 1) {
+      chatsId.map(id => {
+        bot.sendMessage(id, "Обнаружен дым");
+      });
+      mq2 = req.headers.mq2;
+      history.mq2.HIGH.push(new Date());
+    } else {
+      mq2 = req.headers.mq2;
+      history.mq2.LOW.push(new Date());
+    }
+  }
+  res.sendStatus(200);
+});
+
+app.get("/mq4", (req, res) => {
+  console.log("Запрос /mq4, req.headers.mq4: ", req.headers.mq4);
+
+  if (mq4 != req.headers.mq4) {
+    if (req.headers.mq4 == 1) {
+      chatsId.map(id => {
+        bot.sendMessage(id, "Обнаружена утечка газа");
+      });
+      mq4 = req.headers.mq4;
+      history.mq4.HIGH.push(new Date());
+    } else {
+      mq4 = req.headers.mq4;
+      history.mq4.LOW.push(new Date());
+    }
+  }
+  res.sendStatus(200);
+});
+
+app.get("/temp", (req, res) => {
+  console.log(
+    "Запрос /temp, req.headers.temp & humidity: ",
+    req.headers.temp,
+    req.headers.humidity
+  );
+
+  if (temp != req.headers.temp || humidity != req.headers.humidity) {
+    temp = req.headers.temp;
+    humidity = req.headers.humidity;
+    if (req.headers.temp < 15) {
+      chatsId.map(id => {
+        bot.sendMessage(id, `Низкая температура: ${req.headers.temp} градусов`);
+      });
+    }
+    if (req.headers.humidity > 70) {
+      chatsId.map(id => {
+        bot.sendMessage(id, `Повышенная влажность: ${req.headers.temp}%`);
+      });
     }
   }
   res.sendStatus(200);
@@ -134,6 +261,5 @@ app.get("/test", (req, res) => {
 const host = "0.0.0.0";
 const port = process.env.PORT || 3000;
 app.listen(port, host, () => {
-  console.log(process.env.TESTVAR);
   console.log("Listening on Port ", port);
 });
